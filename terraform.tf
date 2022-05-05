2f273573bcb060f13a88a6cb255079c6c24ad0fd
Leonardo <leov.calderon@gmail.com>
2022-05-05 15:27:23 -0500

    test 27

--- terraform.tf
+++ terraform.tf
@@ -26,45 +26,45 @@ provider "aws" {
 
 
 
-resource "aws_lambda_function" "hello_world" {
-  source = "terraform-aws-modules/lambda/aws"
+# resource "aws_lambda_function" "hello_world" {
+#   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "my-lambda-GO"
-  description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
+#   function_name = "my-lambda-GO"
+#   description   = "My awesome lambda function"
+#   handler       = "index.lambda_handler"
+#   runtime       = "python3.8"
 
-  source_path = "../src/lambda-function"
+#   source_path = "../src/lambda-function"
 
-  role = aws_iam_role.lambda_exec.arn
+#   role = aws_iam_role.lambda_exec.arn
   
 
-  tags = {
-    Name = "my-lambda-GO"
-  }
+#   tags = {
+#     Name = "my-lambda-GO"
+#   }
 
-}
+# }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda"
+# resource "aws_iam_role" "lambda_exec" {
+#   name = "serverless_lambda"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Sid    = ""
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+# resource "aws_iam_role_policy_attachment" "lambda_policy" {
+#   role       = aws_iam_role.lambda_exec.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# }
 
 
