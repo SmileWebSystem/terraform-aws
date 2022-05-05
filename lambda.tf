0e369acf5034e4a4568ab9776c9b80ecc7e159fa
Leonardo <leov.calderon@gmail.com>
2022-05-05 14:50:12 -0500

    test 24

--- lambda.tf
+++ lambda.tf
@@ -7,8 +7,31 @@ module "lambda_function" {
   runtime       = "python3.8"
 
   source_path = "../src/lambda-function"
+  role = aws_iam_role.lambda_exec.arn
 
   tags = {
     Name = "my-lambda-GO"
   }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
