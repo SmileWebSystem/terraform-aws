44be25b088cca4bb4f02cf17007f8eef975da3dc
Leonardo <leov.calderon@gmail.com>
2022-05-05 14:27:36 -0500

    test 22

--- lambda.tf
+++ lambda.tf
@@ -6,7 +6,7 @@ module "lambda_function" {
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
 
-  source_path = "../src/lambda-function1"
+  source_path = "../src/lambda-function"
 
   tags = {
     Name = "my-lambda-GO"
