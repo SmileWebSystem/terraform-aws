31a51f9d87b464fc9c8c3ffc7c5212df7537cb68
Leonardo <leov.calderon@gmail.com>
2022-05-05 14:14:07 -0500

    .

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "my-lambda1"
+  function_name = "my-lambda-GO"
   description   = "My awesome lambda function"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
